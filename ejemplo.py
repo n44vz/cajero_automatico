cc9eea4c93bfb54fa032474e68897271010795c8
n44vz <nvz.classes@gmail.com>
2026-01-20 20:30:00 +0000

    Agrega ejemplo funciones

---
+++ ejemplo.py
@@ -0,0 +1,10 @@
+nombre = "Nayib"
+
+def saludar(nombre):
+    """Esta función saluda a la persona cuyo nombre se pasa como parámetro.
+        nombre: Nombre de la persona a la que saludamos.
+    """
+    nombre = "Yinny"
+    return print(f"Hola {nombre}")
+
+print()
